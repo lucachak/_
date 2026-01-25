265d826dae3f278b7f2717928ef03a223e28bffb
lucachak <lucas.lucachak9@gmail.com>
2026-01-25 05:38:41 +0000

    updating

--- core/settings.py
+++ core/settings.py
@@ -136,6 +136,9 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "Static"]  
 STATIC_ROOT = BASE_DIR / "staticfiles"  # new
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / "media"
+
 AUTH_USER_MODEL = 'Accounts.User'
 
 
--- core/urls.py
+++ core/urls.py
@@ -12,18 +12,13 @@ urlpatterns = [
     path('reports/', AdminReportsView.as_view(), name='admin_reports'),
     path('settings/', AdminSettingsView.as_view(), name='admin_settings'),
 
-    # Apps Principais
     path("", include("Home.urls")),
-    
-    # CORREÇÃO: Padronizei para minúsculo "orders/"
     path('orders/', include('Orders.urls')), 
     
     path("billing/", include("Billing.urls")),
     
     path("staff/", include("Staff.urls")),
     path("clients/", include("Clients.urls")),
-    
-    # CORREÇÃO: Removida a duplicidade de 'accounts'
     path("accounts/", include("Accounts.urls")),
     
     path("assets/", include("Assets.urls"))
